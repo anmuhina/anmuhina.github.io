9ea5875b87f3b388896e8324d225d4643b640194
anmuhina <112845722+anmuhina@users.noreply.github.com>
2022-10-26 19:35:41 +0300

    Update script7.js

--- script7.js
+++ script7.js
@@ -1,18 +1,127 @@
-let offset=0; //смещение от левого края
-const sliderLine=document.querySelector('.container .slider .slider-line');
+const mobile_count = 2;
+const desktop_count = 4;
+const dot = document.querySelector("svg.dot");
+let count = desktop_count;
 
-document.querySelector('.container .buttons .next').addEventListener('click', function() {
-    offset += 300;
-    if (offset > 4500) {
-        offset = 0;
-    }
-    sliderLine.style.left = -offset + 'px';
-})
+function resize(){
+  let width = document.querySelector(".slide.active").clientWidth;
+  document.querySelectorAll(".slide").forEach(element => element.style.height = width + "px");
+}
 
-document.querySelector('.container .buttons .prev').addEventListener('click', function() {
-    offset -= 300;
-    if (offset < 0) {
-        offset = 4500;
+function init(){
+  dot.remove();
+
+  function updateView(){
+    if (window.outerWidth <= 425){
+      count = mobile_count;
     }
-    sliderLine.style.left = -offset + 'px';
-})
+    document.querySelectorAll(".slide.active").forEach(element => element.classList.remove('active'));
+    document.querySelectorAll(".dot").forEach(element => element.remove());
+    
+    let slides = document.querySelectorAll(".slide");
+
+    for(let i = 0; i < slides.length; i++){
+      document.querySelector(".dots").appendChild(dot.cloneNode(true));
+    }
+
+    let dots = document.querySelectorAll(".dot");
+
+    for(let i = 0; i < Math.min(count, slides.length); i++){
+      slides[i].classList.add('active');
+      dots[i].classList.add('active-dot');
+    }
+    resize();
+  }
+
+  updateView();
+}
+function back_slide(){
+  let slider = document.querySelector(".slides");
+  let last_slide = slider.querySelector('.slide:last-child');
+  last_slide.remove();
+  slider.insertBefore(last_slide, slider.querySelector('.slide'));
+
+  document.querySelectorAll(".slide.active").forEach(element => element.classList.remove('active'));
+
+  let slides = document.querySelectorAll(".slide");
+  for(let i = 0; i < Math.min(count, slides.length); i++){
+    slides[i].classList.add('active');
+  }
+
+  let dots = document.querySelectorAll(".dot");
+  
+  let last_active_dot;
+  for(let i = 0; i < dots.length - 1; i++){
+    if (dots[i].classList.contains("active-dot") && !dots[i + 1].classList.contains("active-dot")){
+      last_active_dot = dots[i];
+      break;
+    }
+  }
+  let first_active_dot;
+  for(let i = dots.length - 1; i > 0; i--){
+    if (dots[i].classList.contains("active-dot") && !dots[i - 1].classList.contains("active-dot")){
+      console.log(i);
+      first_active_dot = dots[i];
+      break;
+    }
+  }
+
+  if (last_active_dot){
+    last_active_dot.classList.remove("active-dot");
+  } else {
+    dots[dots.length - 1].classList.remove("active-dot");
+  }
+
+  if (first_active_dot && first_active_dot.previousElementSibling){
+    first_active_dot.previousElementSibling.classList.add("active-dot");
+  } else {
+    dots[dots.length - 1].classList.add("active-dot");
+  }
+}
+
+function forward_slide(){
+  let slider = document.querySelector(".slides");
+  let first_slide = slider.querySelector('.slide');
+  first_slide.remove();
+  slider.appendChild(first_slide);
+
+  document.querySelectorAll(".slide.active").forEach(element => element.classList.remove('active'));
+
+  let slides = document.querySelectorAll(".slide");
+  for(let i = 0; i < Math.min(count, slides.length); i++){
+    slides[i].classList.add('active');
+  }
+
+  let dots = document.querySelectorAll(".dot");
+  
+  let last_active_dot;
+  for(let i = 0; i < dots.length - 1; i++){
+    if (dots[i].classList.contains("active-dot") && !dots[i + 1].classList.contains("active-dot")){
+      last_active_dot = dots[i];
+      break;
+    }
+  }
+  let first_active_dot;
+  for(let i = dots.length - 1; i > 0; i--){
+    if (dots[i].classList.contains("active-dot") && !dots[i - 1].classList.contains("active-dot")){
+      console.log(i);
+      first_active_dot = dots[i];
+      break;
+    }
+  }
+
+  if (last_active_dot && last_active_dot.nextElementSibling){
+    last_active_dot.nextElementSibling.classList.add("active-dot");
+  } else {
+    dots[0].classList.add("active-dot");
+  }
+
+  if (first_active_dot){
+    first_active_dot.classList.remove("active-dot");
+  } else {
+    dots[0].classList.remove("active-dot");
+  }
+}
+
+window.addEventListener("DOMContentLoaded", init);
+window.addEventListener("resize", resize, true);
