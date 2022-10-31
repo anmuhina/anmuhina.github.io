18bd6f827300d074703a2c7a11b398e895481175
anmuhina <112845722+anmuhina@users.noreply.github.com>
2022-10-31 16:30:07 +0300

    Update script7.js

--- script7.js
+++ script7.js
@@ -1,4 +1,4 @@
-const mobile_count = 2;
+/*const mobile_count = 2;
 const desktop_count = 4;
 const dot = document.querySelector("svg.dot");
 let count = desktop_count;
@@ -131,4 +131,45 @@ function ready() {
     console.log("DOM is ready");
 }
 
-document.addEventListener("DOMContentLoaded",ready);
+document.addEventListener("DOMContentLoaded",ready);*/
+
+
+$( document ).ready(function() {
+ console.log( "ready!" );
+});
+
+$('.responsive').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+    {
+    breakpoint: 1024,
+    settings: {
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    infinite: true,
+    dots: true
+    }
+    },
+    {
+    breakpoint: 600,
+    settings: {
+    slidesToShow: 2,
+    slidesToScroll: 2
+    }
+    },
+    {
+    breakpoint: 480,
+    settings: {
+    slidesToShow: 1,
+    slidesToScroll: 1
+    }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+    ]
+   });
